32fb23a793e3c92200d26f91d4a907d969e0cecf
Rehnuma Naher Sumona <96902436+RehnumaN8@users.noreply.github.com>
2024-06-10 17:09:25 +0600

    Base Driver,URL,TestNG

---
+++ OpenUrl.java
@@ -0,0 +1,15 @@
+package com.rehnumanaher;
+
+import org.testng.annotations.Test;
+
+public class OpenUrl extends BaseDriver{
+	
+	
+	@Test
+	public void open() throws InterruptedException {
+		
+		driver.get("https://www.selenium.dev/downloads/");
+		Thread.sleep(5000);
+
+}
+}
